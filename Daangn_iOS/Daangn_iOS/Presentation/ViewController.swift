8e81eb4d21fd7e7239c50602471c33eaec696bab
김나연 <nanalovesbap@gmail.com>
2024-11-17 17:01:20 +0900

    [Setting/#1] 프로젝트 기초 세팅 및 폴더링

---
+++ Daangn_iOS/Daangn_iOS/Presentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daangn_iOS
+//
+//  Created by 김나연 on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
