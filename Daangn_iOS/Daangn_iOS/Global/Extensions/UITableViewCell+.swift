e5358ee470cc7d05ee072908a8e4da9ad0473381
ChoiYouBin <cub8533@naver.com>
2024-11-21 18:23:57 +0900

    [Feat/#9] UITableView+ Extension 구현
    className 접근하는 extension

---
+++ Daangn_iOS/Daangn_iOS/Global/Extensions/UITableViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+.swift
+//  Daangn_iOS
+//
+//  Created by 최유빈 on 11/21/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    
+    static var className: String {
+        return String(describing: self)
+    }
+}
